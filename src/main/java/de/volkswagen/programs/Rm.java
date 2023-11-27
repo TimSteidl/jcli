279da7bc23e2a07114d49d0999582f16018c5c9e
Thnnndrrrrrr <tsteidl@duck.com>
2023-11-27 15:29:17 +0100

    implemented rm with good feedback.

--- src/main/java/de/volkswagen/programs/Rm.java
+++ src/main/java/de/volkswagen/programs/Rm.java
@@ -1,4 +1,18 @@
 package de.volkswagen.programs;
 
-public class Rm {
+public class Rm implements CommandlineProgram {
+    @Override
+    public void run(String[] args) {
+        if (args[0].contains("-help")) {
+            help();
+            return;
+        }
+        String path = args[0];
+        boolean deleted = InMemoryCacheSingleton.getInstance().getCurrentPath().resolve(path).toFile().delete();
+        if (deleted) {
+            System.out.println("File deleted");
+        } else {
+            System.out.println("File not found");
+        }
+    }
 }
