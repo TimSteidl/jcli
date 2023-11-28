e2a2e7b8bece98a039e7c66a88692c8d47030434
Thnnndrrrrrr <tsteidl@duck.com>
2023-11-28 13:29:15 +0100

    Fixed cd accessing the wrong argument

--- src/main/java/de/volkswagen/programs/Cd.java
+++ src/main/java/de/volkswagen/programs/Cd.java
@@ -5,17 +5,16 @@ public class Cd implements CommandlineProgram {
 
     @Override
     public void run(String[] args) {
-        if (args[0].contains("-help")) {
+        if (args[1].contains("-help")) {
             help();
             return;
         }
-        System.out.println("arg");
         if (args.length == 1) {
             System.out.println("No path given");
             return;
         }
         String path = args[1];
-        if (args[0].equals("..")) {
+        if (args[1].equals("..")) {
             InMemoryCacheSingleton.getInstance().setCurrentPath(InMemoryCacheSingleton.getInstance().getCurrentPath().getParent());
         } else {
             InMemoryCacheSingleton.getInstance().setCurrentPath(InMemoryCacheSingleton.getInstance().getCurrentPath().resolve(path));
